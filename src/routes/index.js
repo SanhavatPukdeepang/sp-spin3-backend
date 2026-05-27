978e58b22469af71a7723274ccc14b9a02610e84
Korchat123 <korchat123@gmail.com>
2026-05-27 14:19:55 +0700

    connect payment

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { router as authRouter } from './auth.js';
 import { router as orderRouter } from './order.js';
+import { router as paymentRouter } from './payment.js';
 
 export const router = Router();
 
 router.use('/auth', authRouter);
 router.use('/orders', orderRouter);
+router.use('/payments', paymentRouter);
