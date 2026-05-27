f71ca39ced40e144034ad911cac5ef372efb7673
Korchat123 <korchat123@gmail.com>
2026-05-27 15:32:30 +0700

    update cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-  origin: 'https://sp-spin3-frontend.vercel.app',
+  origin: ['https://sp-spin3-frontend.vercel.app', 'http://localhost:5173'],
   credentials: true
 }))
 app.use(express.json())
